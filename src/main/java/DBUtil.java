62da06357e207e858c2e3de84eafc3402f1b2888
udoyen <datameshprojects@gmail.com>
2019-01-25 11:25:41 +0100

    Done with transaction management.

--- src/main/java/DBUtil.java
+++ src/main/java/DBUtil.java
@@ -1,5 +1,6 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -9,10 +10,20 @@ public class DBUtil {
     private static final String mySqlUser = "root";
     private static final String mySqlPwd = "admin";
     private static final String mySQLCS = "jdbc:mysql://172.17.0.2:3306/employees";
+    private static final String mySQLCSBank = "jdbc:mysql://172.17.0.2:3306/newbank";
+
 
     static Connection getConnection(DBType dbType) throws SQLException {
         if (dbType == DBType.MYSQLDB) {
             return DriverManager.getConnection(mySQLCS, mySqlUser, mySqlPwd);
+        }
+
+        return null;
+    }
+
+    static Connection getConnection(DBType dbType, String bank) throws SQLException {
+        if (dbType == DBType.MYSQLDB) {
+            return DriverManager.getConnection(mySQLCSBank, mySqlUser, mySqlPwd);
         }
 
         return null;
@@ -28,7 +39,6 @@ public class DBUtil {
     }
 
     /**
-     *
      * @param dateFormat string
      * @return java sql date
      * @throws ParseException error
@@ -37,6 +47,50 @@ public class DBUtil {
         java.util.Date eDate = new SimpleDateFormat("yyyy-MM-dd").parse(dateFormat);
 
         return new java.sql.Date(eDate.getTime());
+
+    }
+
+    static void recordTransaction(Connection conn, PreparedStatement pstmt,
+                                  double amount, int accId, String transacType
+    ) {
+        String command;
+        try {
+            if (transacType.equalsIgnoreCase("CDT")) {
+                command = "INSERT INTO acc_transaction(AMOUNT, FUNDS_AVAIL_DATE, TXN_DATE, TXN_TYPE_CD, ACCOUNT_ID, EXECUTION_BRANCH_ID, TELLER_EMP_ID)" +
+                        " values(?, ?, ?, ?, ?, ?, ?)";
+                pstmt = conn.prepareStatement(command);
+                pstmt.setDouble(1, amount);
+                pstmt.setTimestamp(2, new java.sql.Timestamp(new java.util.Date().getTime()));
+                pstmt.setTimestamp(3, new java.sql.Timestamp(new java.util.Date().getTime()));
+                pstmt.setString(4, transacType);
+                pstmt.setInt(5, accId);
+                pstmt.setString(6, null);
+                pstmt.setString(7, null);
+                if (pstmt.execute()) {
+                    System.out.println("The Credit Transaction was Successful!");
+                }
+            } else if (transacType.equalsIgnoreCase("WD")) {
+
+                command = "INSERT INTO acc_transaction(AMOUNT, FUNDS_AVAIL_DATE, TXN_DATE, TXN_TYPE_CD, ACCOUNT_ID, EXECUTION_BRANCH_ID, TELLER_EMP_ID)" +
+                        " values(?, ?, ?, ?, ?, ?, ?)";
+                pstmt = conn.prepareStatement(command);
+                pstmt.setDouble(1, amount);
+                pstmt.setTimestamp(2, new java.sql.Timestamp(new java.util.Date().getTime()));
+                pstmt.setTimestamp(3, new java.sql.Timestamp(new java.util.Date().getTime()));
+                pstmt.setString(4, transacType);
+                pstmt.setInt(5, accId);
+                pstmt.setString(6, null);
+                pstmt.setString(7, null);
+                if (pstmt.execute()) {
+                    System.out.println("The Withdrawal Transaction was Successful!");
+                }
+            } else {
+                System.out.println("The Transaction Type isn't Known!");
+            }
+        } catch (SQLException e) {
+            showErrorMessage(e);
+        }
+
 
     }
 
---
+++ src/main/java/TestTransactionManagement.java
@@ -0,0 +1,79 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class TestTransactionManagement {
+
+    public static void main(String[] args) {
+        try {
+            Connection conn = DBUtil.getConnection(DBType.MYSQLDB, "bank");
+            // Prevent the database from committing the transaction results
+            Objects.requireNonNull(conn).setAutoCommit(false);
+
+            PreparedStatement pstmt;
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("GTBank Transactions");
+            System.out.println("----------------------");
+            System.out.println("Enter Account # to Debit :");
+            int fromAccNum = Integer.parseInt(scanner.nextLine());
+            System.out.println("Enter Amount to Transfer : ");
+            double amount = Double.parseDouble(scanner.nextLine());
+            System.out.println("Enter Account Credit : ");
+            int toAccNum = Integer.parseInt(scanner.nextLine());
+
+            String withdrawSQL = "UPDATE account SET AVAIL_BALANCE = AVAIL_BALANCE - ? WHERE ACCOUNT_ID = ?";
+            pstmt = Objects.requireNonNull(conn).prepareStatement(withdrawSQL);
+            pstmt.setDouble(1, amount);
+            pstmt.setInt(2, fromAccNum);
+            int wd = pstmt.executeUpdate();
+
+            // Update the transaction table
+            if(wd != 0) {
+                DBUtil.recordTransaction(conn, pstmt, amount, fromAccNum, "WD");
+            }
+
+            String depositSQL = "UPDATE account SET AVAIL_BALANCE = AVAIL_BALANCE + ? WHERE ACCOUNT_ID = ?";
+            pstmt = conn.prepareStatement(depositSQL);
+            pstmt.setDouble(1, amount);
+            pstmt.setInt(2, toAccNum);
+            int cdt = pstmt.executeUpdate();
+
+            // Update the transaction table
+            if(cdt != 0) {
+                DBUtil.recordTransaction(conn, pstmt, amount, toAccNum, "CDT");
+            }
+
+            String sql = "SELECT AVAIL_BALANCE FROM account WHERE ACCOUNT_ID = ?";
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1, fromAccNum);
+            ResultSet rs = pstmt.executeQuery();
+            double balanceAmount = 0;
+            if (rs.next()) {
+                balanceAmount = rs.getDouble("AVAIL_BALANCE");
+            }
+
+            // Check to make sure the min balance
+            // in the account ot debit is more than
+            // N5000
+            if(balanceAmount >= 5000) {
+                conn.commit();
+                System.out.println("Amount Transferred Successfully...");
+            } else {
+                conn.rollback();
+                System.out.println("Insufficient Funds : " + balanceAmount + " Transactions Rolleback..");
+            }
+
+            scanner.close();
+            pstmt.close();
+            conn.close();
+
+
+        } catch (SQLException e) {
+            DBUtil.showErrorMessage(e);
+        }
+    }
+}
